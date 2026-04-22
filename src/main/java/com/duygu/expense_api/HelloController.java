a0dd0641e268fdd81e99bfe9cd36bc1573c235b6
duygutopluu <toplud5118679_aehit@students.vizja.pl>
2026-04-22 20:54:11 +0200

    feat: add expenses list endpoint

--- src/main/java/com/duygu/expense_api/HelloController.java
+++ src/main/java/com/duygu/expense_api/HelloController.java
@@ -1,5 +1,6 @@
 package com.duygu.expense_api;
-
+import java.util.List;
+import java.util.ArrayList;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -18,4 +19,22 @@ public class HelloController {
         e.amount = 50;
         return e;
     }
+
+    @GetMapping("/expenses")
+public List<Expense> getExpenses() {
+    List<Expense> list = new ArrayList<>();
+
+    Expense e1 = new Expense();
+    e1.title = "Coffee";
+    e1.amount = 50;
+
+    Expense e2 = new Expense();
+    e2.title = "Taxi";
+    e2.amount = 120;
+
+    list.add(e1);
+    list.add(e2);
+
+    return list;
+}
 }
